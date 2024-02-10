c40583581d58d870e0227cfee96fe0ca86b7f51c
roemil <emil.rosenberg@hotmail.com>
2024-02-10 17:14:04 +0100

    Rename decoder suite

--- ccredis/test/RespHandlerDecodeTest.cpp
+++ ccredis/test/RespHandlerDecodeTest.cpp
@@ -8,70 +8,70 @@
 #include "RespDecoder.h"
 #include "Resp.h"
 
-class RespHandlerDecodeTest : public testing::Test {
+class RespDecoderTest : public testing::Test {
 protected:
     RespDecoder rh {};
 };
 
-TEST_F(RespHandlerDecodeTest, SimpleString)
+TEST_F(RespDecoderTest, SimpleString)
 {
     constexpr std::string_view simpleString = { "+OK\r\n" };
     EXPECT_EQ(std::string_view { "OK" }, rh.decode(simpleString).second.string_);
 }
 
-TEST_F(RespHandlerDecodeTest, PingString)
+TEST_F(RespDecoderTest, PingString)
 {
     constexpr std::string_view simpleString = { "+PING\r\n" };
     EXPECT_EQ(std::string_view { "PING" }, rh.decode(simpleString).second.string_);
 }
 
-TEST_F(RespHandlerDecodeTest, IllegalString)
+TEST_F(RespDecoderTest, IllegalString)
 {
     constexpr std::string_view simpleString = { "+OK" };
     EXPECT_THROW(static_cast<void>(rh.decode(simpleString)), std::invalid_argument);
 }
 
-TEST_F(RespHandlerDecodeTest, Error)
+TEST_F(RespDecoderTest, Error)
 {
     EXPECT_EQ(std::string_view { "My Error" }, rh.decode("-My Error\r\n").second.error_);
 }
 
-TEST_F(RespHandlerDecodeTest, IntegerNoSign)
+TEST_F(RespDecoderTest, IntegerNoSign)
 {
     EXPECT_EQ(1000, rh.decode(":1000\r\n").second.integer_);
 }
 
-TEST_F(RespHandlerDecodeTest, IntegerNegative)
+TEST_F(RespDecoderTest, IntegerNegative)
 {
     EXPECT_EQ(-1000, rh.decode(":-1000\r\n").second.integer_);
 }
 
-TEST_F(RespHandlerDecodeTest, IntegerPos)
+TEST_F(RespDecoderTest, IntegerPos)
 {
     EXPECT_EQ(1000, rh.decode(":+1000\r\n").second.integer_);
 }
 
-TEST_F(RespHandlerDecodeTest, BulkString)
+TEST_F(RespDecoderTest, BulkString)
 {
     EXPECT_EQ(std::string_view { "ping" }, rh.decode("$4\r\nping\r\n").second.string_);
 }
 
-TEST_F(RespHandlerDecodeTest, BulkStringLonger)
+TEST_F(RespDecoderTest, BulkStringLonger)
 {
     EXPECT_EQ(std::string_view { "hello world" }, rh.decode("$11\r\nhello world\r\n").second.string_);
 }
 
-TEST_F(RespHandlerDecodeTest, Null)
+TEST_F(RespDecoderTest, Null)
 {
     EXPECT_EQ(std::string_view { "null" }, rh.decode("$-1\r\n").second.string_);
 }
 
-TEST_F(RespHandlerDecodeTest, ArraySingleInt)
+TEST_F(RespDecoderTest, ArraySingleInt)
 {
     EXPECT_EQ(RedisRespRes { .array_ = { std::vector<RedisRespRes> { RedisRespRes { .integer_ = 1 } } } }, rh.decode("*1\r\n:1\r\n").second);
 }
 
-TEST_F(RespHandlerDecodeTest, ArrayTwoInts)
+TEST_F(RespDecoderTest, ArrayTwoInts)
 {
     RedisRespRes redisRespInt1 = RedisRespRes { .integer_ = 1 };
     RedisRespRes redisRespInt2 = RedisRespRes { .integer_ = 2 };
@@ -79,7 +79,7 @@ TEST_F(RespHandlerDecodeTest, ArrayTwoInts)
     EXPECT_EQ(redisRespArr, rh.decode("*2\r\n:1\r\n:2\r\n").second);
 }
 
-TEST_F(RespHandlerDecodeTest, ArrayTwoIntsBulkString)
+TEST_F(RespDecoderTest, ArrayTwoIntsBulkString)
 {
     RedisRespRes redisRespInt1 = RedisRespRes { .integer_ = 1 };
     RedisRespRes redisRespInt2 = RedisRespRes { .integer_ = 2 };
@@ -88,7 +88,7 @@ TEST_F(RespHandlerDecodeTest, ArrayTwoIntsBulkString)
     EXPECT_EQ(redisRespArr, rh.decode("*3\r\n:1\r\n$4\r\nPING\r\n:2\r\n").second);
 }
 
-TEST_F(RespHandlerDecodeTest, ArrayTwoIntsLongBulkString)
+TEST_F(RespDecoderTest, ArrayTwoIntsLongBulkString)
 {
     RedisRespRes redisRespInt1 = RedisRespRes { .integer_ = 1 };
     RedisRespRes redisRespInt2 = RedisRespRes { .integer_ = 2 };
@@ -97,14 +97,14 @@ TEST_F(RespHandlerDecodeTest, ArrayTwoIntsLongBulkString)
     EXPECT_EQ(redisRespArr, rh.decode("*3\r\n:1\r\n$11\r\nhello world\r\n:2\r\n").second);
 }
 
-TEST_F(RespHandlerDecodeTest, NestedArr)
+TEST_F(RespDecoderTest, NestedArr)
 {
     RedisRespRes redisRespInt = RedisRespRes { .integer_ = 2 };
     RedisRespRes redisRespArr = RedisRespRes { .array_ = std::vector<RedisRespRes> { RedisRespRes { .array_ = std::vector<RedisRespRes> { redisRespInt } } } };
     EXPECT_EQ(redisRespArr, rh.decode("*1\r\n*1\r\n:2\r\n\r\n").second);
 }
 
-TEST_F(RespHandlerDecodeTest, Map)
+TEST_F(RespDecoderTest, Map)
 {
     RedisRespRes redisRespInt = RedisRespRes { .integer_ = 2 };
     std::map<std::string_view, RedisRespRes> map {};
@@ -113,7 +113,7 @@ TEST_F(RespHandlerDecodeTest, Map)
     EXPECT_EQ(redisRespMap, rh.decode("%1\r\n+key\r\n:2\r\n\r\n").second);
 }
 
-TEST_F(RespHandlerDecodeTest, HelloHandshake)
+TEST_F(RespDecoderTest, HelloHandshake)
 {
     RedisRespRes redisRespInt = RedisRespRes { .integer_ = 3 };
     RedisRespRes redisRespHello = RedisRespRes { .string_ = "HELLO" };
