3a3afb92e1e3cae9086f5e3764fbeb27669c2fdb
roemil <emil.rosenberg@hotmail.com>
2024-02-10 17:12:18 +0100

    UT for SET cmd

--- ccredis/test/RespCommandConverterTest.cpp
+++ ccredis/test/RespCommandConverterTest.cpp
@@ -1,3 +1,4 @@
+#include "Commands.h"
 #include "RespDecoder.h"
 #include "Resp.h"
 
@@ -24,21 +25,6 @@ TEST_F(RespCommandConverterTest, Ping)
     EXPECT_NO_THROW(std::get<CommandPing>(rh.convertToCommands(rawCommand)[0]));
 }
 
-// TEST_F(RespCommandConverterTest, PingArray)
-// {
-//     RedisRespRes rawCommand{.array_ = std::vector<RedisRespRes> {RedisRespRes{.string_ {"PING"}}}};
-//     EXPECT_EQ(Command{.kind_ = CommandKind::PING}, rh.convertToCommands(rawCommand)[0]);
-// }
-
-// TEST_F(RespCommandConverterTest, MultiplePingsArray)
-// {
-//     RedisRespRes rawCommand{.array_ = std::vector<RedisRespRes> { RedisRespRes{.string_ {"PING"}}, RedisRespRes{.string_ {"PING"}} }};
-//     const auto commands = rh.convertToCommands(rawCommand);
-//     EXPECT_EQ(2, commands.size());
-//     EXPECT_EQ(Command{.kind_ = CommandKind::PING}, commands[0]);
-//     EXPECT_EQ(Command{.kind_ = CommandKind::PING}, commands[1]);
-// }
-
 TEST_F(RespCommandConverterTest, ArrayCommandWithPayload)
 {
     RedisRespRes rawCommand { .array_ = std::vector<RedisRespRes> { RedisRespRes { .string_ { "HELLO" } }, RedisRespRes { .string_ { "3" } } } };
@@ -47,12 +33,10 @@ TEST_F(RespCommandConverterTest, ArrayCommandWithPayload)
     EXPECT_EQ("3", std::get<CommandHello>(commands[0]).version_);
 }
 
-// TEST_F(RespCommandConverterTest, SET)
-// {
-//     RedisRespRes rawCommand { .array_ = std::vector<RedisRespRes> { RedisRespRes { .string_ { "SET" } }, RedisRespRes { .string_ { "key" } }, RedisRespRes { .string_ { "value" } } } };
-//     const auto commands = rh.convertToCommands(rawCommand);
-//     EXPECT_EQ(1, commands.size());
-//     EXPECT_EQ(CommandKind::SET, commands[0].kind_);
-//     EXPECT_EQ("key", std::get<std::string_view>(commands[0].payload_.value()[0]));
-//     EXPECT_EQ("value", std::get<std::string_view>(commands[0].payload_.value()[1]));
-// }
+TEST_F(RespCommandConverterTest, SET)
+{
+    RedisRespRes rawCommand { .array_ = std::vector<RedisRespRes> { RedisRespRes { .string_ { "SET" } }, RedisRespRes { .string_ { "key" } }, RedisRespRes { .string_ { "value" } } } };
+    const auto commands = rh.convertToCommands(rawCommand);
+    EXPECT_EQ("key", std::get<CommandSet>(commands[0]).key_);
+    EXPECT_EQ("value", std::get<CommandSet>(commands[0]).value_);
+}
