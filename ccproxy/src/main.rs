81bbdf6b25ab5b3dd28b7f1c7d16bc9bc306a6df
Emil Rosenberg <emil.rosenberg@acorntechnology.se>
2024-09-09 14:01:04 +0200

    Proxy can forward and reject connections due to blacklist

---
+++ ccproxy/src/main.rs
@@ -0,0 +1,128 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::fs;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::{Method, Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+#[derive(Debug, Clone)]
+pub struct Proxy<S> {
+    inner: S,
+}
+impl<S> Proxy<S> {
+    pub fn new(inner: S) -> Self {
+        Proxy { inner }
+    }
+}
+type Req = Request<hyper::body::Incoming>;
+
+impl<S> hyper::service::Service<Req> for Proxy<S>
+where
+    S: hyper::service::Service<Req>,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = S::Future;
+    fn call(&self, req: Req) -> Self::Future {
+        println!(
+            "processing request from: {} {}",
+            req.method(),
+            req.uri().path()
+        );
+        self.inner.call(req)
+    }
+}
+
+async fn forward_proxy(
+    remote: SocketAddr,
+    req: Request<hyper::body::Incoming>,
+    rules: Arc<Vec<String>>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    println!("{:?}", req);
+    println!("Remote: {:?}", remote);
+    let headers = req.headers();
+    if !headers.contains_key("proxy-connection") {
+        return Ok(Response::new(Full::new(Bytes::from(
+            "Not a proxy request!",
+        ))));
+    }
+    if *req.method() != Method::GET {
+        return Ok(Response::new(Full::new(Bytes::from("Not a GET request!"))));
+    }
+
+    if rules.contains(&req.uri().to_string()) {
+        return Ok(Response::new(Full::new(Bytes::from(
+            StatusCode::FORBIDDEN.as_str(),
+        ))));
+    }
+
+    let c = reqwest::Client::new();
+    let r = c
+        .get(req.uri().to_string())
+        .header("User-Agent", headers.get("User-Agent").unwrap())
+        .header("X-Forwarded-For", remote.to_string());
+    // TODO: Make sure it is keep-alive'd
+    let req = r.build().expect("Failed to build request");
+
+    let b = match c.execute(req).await {
+        Ok(r) => r.bytes().await,
+        Err(e) => {
+            eprintln!("Failed to forward request. Error: {}", e);
+            // TODO: Return proper error codes
+            Ok(Bytes::from("Failed to forward req"))
+        }
+    };
+
+    let response = Response::new(Full::new(b.unwrap()));
+    println!("Response: {:?}", response);
+    Ok(response)
+}
+
+fn parse_blacklist() -> Vec<String> {
+    let contents =
+        fs::read_to_string("src/blacklist.txt").expect("Should have been able to read the file");
+    println!("{}", contents);
+    contents
+        .lines()
+        .into_iter()
+        .map(|line| line.to_string())
+        .collect()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8089));
+
+    let listener = TcpListener::bind(addr).await?;
+
+    let blacklist = Arc::new(parse_blacklist());
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+        let blacklist_c = blacklist.clone();
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            let remote = io.inner().peer_addr().expect("Remote address must exist");
+            // Finally, we bind the incoming connection to our `hello` service
+            let svc = hyper::service::service_fn(move |req| {
+                forward_proxy(remote, req, blacklist_c.clone() )
+            });
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, svc)
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
