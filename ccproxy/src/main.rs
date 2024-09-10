34d303c8485c5d5428e36ef8a49f623f331fbef5
Emil Rosenberg <emil.rosenberg@acorntechnology.se>
2024-09-10 10:16:14 +0200

    fix error response for blacklisted addresses

--- ccproxy/src/main.rs
+++ ccproxy/src/main.rs
@@ -2,11 +2,15 @@ use std::convert::Infallible;
 use std::fs;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use bytes::Bytes;
 
+use http_body_util::combinators::BoxBody;
+use http_body_util::BodyExt;
+use hyper::client::conn::http1::Builder;
 use hyper::server::conn::http1;
-use hyper::{Method, Request, Response, StatusCode};
+use hyper::{Error, Method, Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
-use log::{error, info};
+use log::error;
 use tokio::net::TcpListener;
 
 #[derive(Debug, Clone)]
@@ -37,19 +41,18 @@ where
     }
 }
 
-fn create_response<T>(body: T, http_code: StatusCode) -> Response<T> {
-    let response = Response::new(body);
-    let (mut parts, body) = response.into_parts();
-
-    parts.status = http_code;
-    Response::from_parts(parts, body)
+fn create_response(http_code: StatusCode) -> Response<BoxBody<Bytes, Error>> {
+    let body = Bytes::from(http_code.as_str().to_string());
+    let mut response = Response::new(BoxBody::<Bytes, Error>::new(http_body_util::Full::new(body).map_err(|e| match e {})));
+    *response.status_mut() = http_code;
+    response
 }
 
 async fn forward_proxy(
     local_socket: SocketAddr,
     req: Request<hyper::body::Incoming>,
     rules: Arc<Vec<String>>,
-) -> Result<Response<String>, Infallible> {
+) -> Result<Response<BoxBody<Bytes, Error>>, Infallible> {
     println!("Req {:?}", req);
     println!("Host {:?}", req.uri().host());
     println!("Port {:?}", req.uri().port());
@@ -57,7 +60,7 @@ async fn forward_proxy(
     println!("Remote {}", local_socket);
     let headers = req.headers();
     if !headers.contains_key("proxy-connection") {
-        let resp = create_response("".to_string(), StatusCode::BAD_REQUEST);
+        let resp = create_response(StatusCode::BAD_REQUEST);
         error!(
             "Error: Missing proxy-connection. Client: {}, Request URL: {}, StatusCode: {}",
             local_socket.to_string(),
@@ -67,7 +70,7 @@ async fn forward_proxy(
         return Ok(resp);
     }
     if *req.method() != Method::GET && *req.method() != Method::CONNECT {
-        let resp = create_response("".to_string(), StatusCode::BAD_REQUEST);
+        let resp = create_response(StatusCode::BAD_REQUEST);
         error!(
             "Error: Not a Get or Connect request. Client: {}, Request URL: {}, Method: {} , StatusCode: {}",
             local_socket.to_string(),
@@ -79,9 +82,9 @@ async fn forward_proxy(
     }
 
     if let Some(response) = is_on_blacklist(rules, &req, local_socket) {
-        return response;
+        return Ok(response);
     }
-    
+
     if *req.method() == Method::CONNECT {
         let addr = req.uri().authority().unwrap().to_string();
         tokio::task::spawn(async move {
@@ -95,53 +98,48 @@ async fn forward_proxy(
             }
         });
         // Must return a empty body to client then the connection will be upgraded.
-        return Ok(Response::<String>::default());
+        return Ok(Response::default());
     }
 
-    let c = reqwest::Client::new();
-    let r = c
-        .get(&req.uri().to_string())
-        .header("User-Agent", headers.get("User-Agent").unwrap())
-        .header("X-Forwarded-For", local_socket.to_string());
-    // TODO: Make sure it is keep-alive'd
-    let forward_req = r.build().expect("Failed to build request");
-
-    let b = match c.execute(forward_req).await {
-        Ok(r) => r.text().await,
-        Err(e) => {
-            eprintln!("Failed to forward request. Error: {}", e);
-            let resp = create_response("".to_string(), StatusCode::BAD_GATEWAY);
-            error!(
-                "Failed to forward request. Error: {}, Client: {}, StatusCode: {}",
-                e,
-                local_socket.to_string(),
-                resp.status()
-            );
-            return Ok(resp);
-        }
-    };
-
-    // TODO: Add proper headers
-    let response = Response::new(b.unwrap());
-    info!(
-        "Client: {}, Request URL: {}, StatusCode: {}",
-        local_socket.to_string(),
-        req.uri(),
-        response.status()
-    );
-    Ok(response)
+    Ok(forward_request(req).await?)
 }
 
-fn is_on_blacklist(rules: Arc<Vec<String>>, req: &Request<hyper::body::Incoming>, local_socket: SocketAddr) -> Option<Result<Response<String>, Infallible>> {
+async fn forward_request(req: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, Error>>, Infallible> {
+    let addr = req.uri().host().unwrap();
+    let port = req.uri().port_u16().unwrap_or(80);
+    let stream = tokio::net::TcpStream::connect((addr,port))
+        .await
+        .expect("Remote not available");
+    let io = TokioIo::new(stream);
+    let (mut sender, conn) = Builder::new()
+        .preserve_header_case(true)
+        .title_case_headers(true)
+        .handshake(io)
+        .await
+        .unwrap();
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
+    let resp = sender.send_request(req).await.unwrap();
+    Ok(resp.map(|body| body.boxed()))
+}
+
+fn is_on_blacklist(
+    rules: Arc<Vec<String>>,
+    req: &Request<hyper::body::Incoming>,
+    local_socket: SocketAddr,
+) -> Option<Response<BoxBody<Bytes, Error>>> {
     if rules.contains(&req.uri().host().expect("Host must exist").to_string()) {
-        let resp = create_response("".to_string(), StatusCode::FORBIDDEN);
+        let resp = create_response(StatusCode::FORBIDDEN);
         error!(
             "Error: Forbidden URI. Client: {}, Request URL: {}, StatusCode: {}",
             local_socket.to_string(),
             &req.uri(),
             resp.status()
         );
-        return Some(Ok(resp));
+        return Some(resp);
     }
     None
 }
