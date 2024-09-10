a960eeea631bd6df906a8641aaa5975d739a48ec
Emil Rosenberg <emil.rosenberg@acorntechnology.se>
2024-09-10 09:24:24 +0200

    add tls support

--- ccproxy/src/main.rs
+++ ccproxy/src/main.rs
@@ -37,35 +37,40 @@ where
     }
 }
 
-fn create_response<T>(body: T, http_code : StatusCode) -> Response<T> {
+fn create_response<T>(body: T, http_code: StatusCode) -> Response<T> {
     let response = Response::new(body);
-        let (mut parts, body) = response.into_parts();
+    let (mut parts, body) = response.into_parts();
 
-        parts.status = http_code;
-        Response::from_parts(parts, body)
+    parts.status = http_code;
+    Response::from_parts(parts, body)
 }
 
 async fn forward_proxy(
-    remote: SocketAddr,
+    local_socket: SocketAddr,
     req: Request<hyper::body::Incoming>,
     rules: Arc<Vec<String>>,
 ) -> Result<Response<String>, Infallible> {
+    println!("Req {:?}", req);
+    println!("Host {:?}", req.uri().host());
+    println!("Port {:?}", req.uri().port());
+    println!("Authority {:?}", req.uri().authority());
+    println!("Remote {}", local_socket);
     let headers = req.headers();
     if !headers.contains_key("proxy-connection") {
         let resp = create_response("".to_string(), StatusCode::BAD_REQUEST);
         error!(
             "Error: Missing proxy-connection. Client: {}, Request URL: {}, StatusCode: {}",
-            remote.to_string(),
+            local_socket.to_string(),
             &req.uri(),
             resp.status()
         );
         return Ok(resp);
     }
-    if *req.method() != Method::GET {
+    if *req.method() != Method::GET && *req.method() != Method::CONNECT {
         let resp = create_response("".to_string(), StatusCode::BAD_REQUEST);
         error!(
-            "Error: Not a Get request. Client: {}, Request URL: {}, Method: {} , StatusCode: {}",
-            remote.to_string(),
+            "Error: Not a Get or Connect request. Client: {}, Request URL: {}, Method: {} , StatusCode: {}",
+            local_socket.to_string(),
             &req.uri(),
             req.method(),
             resp.status()
@@ -73,22 +78,31 @@ async fn forward_proxy(
         return Ok(resp);
     }
 
-    if rules.contains(&req.uri().to_string()) {
-        let resp = create_response("".to_string(), StatusCode::FORBIDDEN);
-        error!(
-            "Error: Forbidden URI. Client: {}, Request URL: {}, StatusCode: {}",
-            remote.to_string(),
-            &req.uri(),
-            resp.status()
-        );
-        return Ok(resp);
+    if let Some(response) = is_on_blacklist(rules, &req, local_socket) {
+        return response;
+    }
+    
+    if *req.method() == Method::CONNECT {
+        let addr = req.uri().authority().unwrap().to_string();
+        tokio::task::spawn(async move {
+            match hyper::upgrade::on(req).await {
+                Ok(upgraded) => {
+                    if let Err(e) = tunnel(upgraded, addr).await {
+                        eprintln!("server io error: {}", e);
+                    };
+                }
+                Err(e) => eprintln!("upgrade error: {}", e),
+            }
+        });
+        // Must return a empty body to client then the connection will be upgraded.
+        return Ok(Response::<String>::default());
     }
 
     let c = reqwest::Client::new();
     let r = c
         .get(&req.uri().to_string())
         .header("User-Agent", headers.get("User-Agent").unwrap())
-        .header("X-Forwarded-For", remote.to_string());
+        .header("X-Forwarded-For", local_socket.to_string());
     // TODO: Make sure it is keep-alive'd
     let forward_req = r.build().expect("Failed to build request");
 
@@ -100,7 +114,7 @@ async fn forward_proxy(
             error!(
                 "Failed to forward request. Error: {}, Client: {}, StatusCode: {}",
                 e,
-                remote.to_string(),
+                local_socket.to_string(),
                 resp.status()
             );
             return Ok(resp);
@@ -111,11 +125,25 @@ async fn forward_proxy(
     let response = Response::new(b.unwrap());
     info!(
         "Client: {}, Request URL: {}, StatusCode: {}",
-        remote.to_string(),
+        local_socket.to_string(),
         req.uri(),
         response.status()
     );
     Ok(response)
+}
+
+fn is_on_blacklist(rules: Arc<Vec<String>>, req: &Request<hyper::body::Incoming>, local_socket: SocketAddr) -> Option<Result<Response<String>, Infallible>> {
+    if rules.contains(&req.uri().host().expect("Host must exist").to_string()) {
+        let resp = create_response("".to_string(), StatusCode::FORBIDDEN);
+        error!(
+            "Error: Forbidden URI. Client: {}, Request URL: {}, StatusCode: {}",
+            local_socket.to_string(),
+            &req.uri(),
+            resp.status()
+        );
+        return Some(Ok(resp));
+    }
+    None
 }
 
 fn parse_blacklist(path: &str) -> Vec<String> {
@@ -125,6 +153,17 @@ fn parse_blacklist(path: &str) -> Vec<String> {
         .into_iter()
         .map(|line| line.to_string())
         .collect()
+}
+
+async fn tunnel(upgraded: hyper::upgrade::Upgraded, addr: String) -> std::io::Result<()> {
+    // Connect to remote server
+    let mut server = tokio::net::TcpStream::connect(addr).await?;
+    let mut upgraded = TokioIo::new(upgraded);
+
+    // Proxying data
+    let _ = tokio::io::copy_bidirectional(&mut upgraded, &mut server).await?;
+
+    Ok(())
 }
 
 #[tokio::main]
@@ -145,7 +184,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             let svc = hyper::service::service_fn(move |req| {
                 forward_proxy(remote, req, blacklist_c.clone())
             });
-            if let Err(err) = http1::Builder::new().serve_connection(io, svc).await {
+            if let Err(err) = http1::Builder::new()
+                .preserve_header_case(true)
+                .title_case_headers(true)
+                .serve_connection(io, svc)
+                .with_upgrades()
+                .await
+            {
                 eprintln!("Error serving connection: {:?}", err);
             }
         });
